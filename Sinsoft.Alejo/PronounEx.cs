69c93daf545d7dbe0832bf93a1ac60e24599baf9
Sinistral2099 <sinistral2099@gmail.com>
2023-11-10 01:10:19 -0600

    Updated He/They and She/They to default to the primary, rather than the secondary.

--- Sinsoft.Alejo/PronounEx.cs
+++ Sinsoft.Alejo/PronounEx.cs
@@ -35,12 +35,12 @@ namespace Sinsoft.Alejo
         public static PronounEx FaeFaer { get { return new PronounEx("faefaer", "Fae/Faer", "Fae", "Faer", "Faer", "Faers", "Faerself", "Were", "Are", false); } }
         public static PronounEx HeHim { get { return new PronounEx("hehim", "He/Him", "He", "Him", "His", "His", "Himself", "Was", "Is", false); } }
         public static PronounEx HeShe { get { return TheyThem; } }
-        public static PronounEx HeThey { get { return TheyThem; } } //Only for legacy support and ease of use interaction. Not in the new API.
+        public static PronounEx HeThey { get { return HeHim; } } //Only for legacy support and ease of use interaction. Not in the new API.
         public static PronounEx ItIts { get { return new PronounEx("itits", "It/Its", "It", "It", "Its", "Its", "Itself", "Was", "Is", false); } }
         public static PronounEx Other { get { return TheyThem; } }
         public static PronounEx PerPer { get { return new PronounEx("perper", "Per/Per", "Per", "Per", "Pers", "Pers", "Perself", "Was", "Is", false); } }
         public static PronounEx SheHer { get { return new PronounEx("sheher", "She/Her", "She", "Her", "Hers", "Hers", "Herself", "Was", "Is", false); } }
-        public static PronounEx SheThey { get { return TheyThem; } } //Only for legacy support and ease of use interaction. Not in the new API.
+        public static PronounEx SheThey { get { return SheHer; } } //Only for legacy support and ease of use interaction. Not in the new API.
         public static PronounEx TheyThem { get { return new PronounEx("theythem", "They/Them", "They", "Them", "Their", "Theirs", "Themself", "Were", "Are", false); } }
         public static PronounEx VeVer { get { return new PronounEx("vever", "Ve/Ver", "Ve", "Ver", "Vis", "Vis", "Verself", "Was", "Is", false); } }
         public static PronounEx XeXem { get { return new PronounEx("xexem", "Xe/Xem", "Xe", "Xem", "Xyr", "Xyrs", "Xemself", "Was", "Is", false); } }
